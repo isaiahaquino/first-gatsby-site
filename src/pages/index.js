347d6973b0882c54baaebc1915476af73c7794c5
Zay Macbook <isaiahaquino1997@gmail.com>
2022-08-05 12:55:50 -1000

    Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
 
@@ -8,6 +9,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle={title}>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage 
+        src='https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large'
+        alt='Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera'
+      />
     </Layout>
   )
 }
